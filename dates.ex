a4a5bcef98890a6e9f94ccb1cd6d295ec019a1d3
Vikram Ramakrishnan <lord.ezar@gmail.com>
2015-03-24 20:56:51 -0400

    Etude 6-2: Julian date converter

--- dates.ex
+++ dates.ex
@@ -9,11 +9,21 @@ defmodule Dates do
   def julian(date) do
     [year, month, day] = date_parts(date)
     days_per_month = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
-
+    total_days = month_total(month, days_per_month, 0) + day
+    cond do
+      is_leap_year(year) and month > 2 -> total_days + 1
+      true -> total_days
+    end
   end
 
-  def month_total(month, days_per_month, total) do
+  @spec month_total(number, [number], number) :: number
 
+  def month_total(1, _days_per_month, total) do
+    total
+  end
+
+  def month_total(month, [h | t], total) do
+    month_total(month - 1, t, total + h)
   end
 
   defp is_leap_year(year) do
